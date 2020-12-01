f400a774ca05037419ece35cf01712f65c56782f
pizisummer <piz-summer@63.com>
2020-12-01 21:49:07 +0800

    第5次更改

--- DemoModule1/src/com/feiw/MainTest2.java
+++ DemoModule1/src/com/feiw/MainTest2.java
@@ -2,6 +2,7 @@ package com.feiw;
 
 public class MainTest2 {
     public static void main(String[] args) {
-        System.out.println("hello ");
+        System.out.println("hello word!!");
+
     }
 }
